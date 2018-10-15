75ac98bc267844c5116cd5c957c146ac4ba18c74
Charles Ibo <charles.ibo.247@my.csun.edu>
2018-10-15 15:37:43 -0700

    Vars

---
+++ option.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Another variable form, everything that you enter in command line after script name
+#Using $1 instead of $@ would only give first option
+echo "I saw you input $@ in the command line"
---
+++ variable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Double quotes allow bash to interpret bash commands
+output=$(pwd)
+echo "Your home dir is ${output}"
